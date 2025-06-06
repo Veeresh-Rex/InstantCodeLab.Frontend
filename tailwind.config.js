25b50b6e06f580c5e5ab08387662bc1fef7250f1
Veeresh Maurya <mauryaveeresh@gmail.com>
2025-06-06 18:25:50 +0530

    update tailwind.config.js to include types directory and modify tsconfig.node.json paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/types/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
