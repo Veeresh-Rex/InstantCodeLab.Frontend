611f2cc61c6e2bef6a9852563068ce3642d401d9
Veeresh Maurya <mauryaveeresh@gmail.com>
2025-06-11 21:19:12 +0530

    fix: update SignalR connection to include withCredentials option

--- src/services/signalRClient.ts
+++ src/services/signalRClient.ts
@@ -1,7 +1,9 @@
 import * as signalR from '@microsoft/signalr';
 
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl(import.meta.env.VITE_CODE_SIGNALR_URL)
+  .withUrl(import.meta.env.VITE_CODE_SIGNALR_URL, {
+    withCredentials: true,
+  })
   .withAutomaticReconnect()
   .configureLogging(signalR.LogLevel.Information)
   .build();
